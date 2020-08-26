c9b66111902ba4088ead1104819e4fa2ebbd48ec
skyaxe97 <skyaxe97@gmail.com>
2020-08-26 11:58:38 +0800

    Add echo of user commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        System.out.println("Hello and Bye!");
+        System.out.println("Hello! What is up sir?");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNextLine()) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            else {
+                System.out.println(line);
+            }
+        }
+
+        System.out.println("Goodbye sir");
     }
 }
