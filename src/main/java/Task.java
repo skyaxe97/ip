cc8301a4a85816b0747f158a37586c82f0de5fcd
skyaxe97 <skyaxe97@gmail.com>
2020-08-27 16:54:37 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public static int taskCount = 0;
+
+    public Task (String description) {
+        this.description = description;
+        this.isDone = false;
+        taskCount++;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone (boolean isDone) {
+        this.isDone = true;
+    }
+}
