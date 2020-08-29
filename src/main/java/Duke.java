4a7194081ffd73f992f3152e22edc08029ca346b
skyaxe97 <42371936+skyaxe97@users.noreply.github.com>
2020-08-29 23:53:11 +0800

    Tweak code to comply with coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,28 +12,21 @@ public class Duke {
             line = in.nextLine();
             if (line.equals("bye")) {
                 break;
-            }
-
-            else if (line.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < Task.taskCount; i++) {
-                    System.out.println((i + 1) + ". [" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
-                }
-            }
-
-            else if (line.startsWith("done ")) {
-                int idx = Integer.parseInt(line.substring(5)) - 1;
-                taskList[idx].markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("\t" + "[" + "\u2713" + "] " + taskList[idx].description);
-            }
-
-            else {
-                taskList[Task.taskCount] = new Task (line);
-                System.out.println("added: " + line);
+            }   else if (line.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < Task.taskCount; i++) {
+                        System.out.println((i + 1) + ". [" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
+                    }
+            }   else if (line.startsWith("done ")) {
+                    int idx = Integer.parseInt(line.substring(5)) - 1;
+                    taskList[idx].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("\t" + "[" + "\u2713" + "] " + taskList[idx].description);
+            }   else {
+                    taskList[Task.taskCount] = new Task (line);
+                    System.out.println("added: " + line);
             }
         }
-
         System.out.println("Goodbye sir");
     }
 }
