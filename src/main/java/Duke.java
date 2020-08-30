56c7827ad2010e9d5e9c1887c59dfcc20c1bc680
skyaxe97 <skyaxe97@gmail.com>
2020-08-31 01:41:24 +0800

    Tweak code due to merge conflict issues

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,16 +16,13 @@ public class Duke {
             if (line.equals("bye")) {
                 break;
             }   else if (line.equals("list")) {
-<<<<<<< HEAD
                     printList(taskList);
             }   else if (line.startsWith("done ")) {
                     doTask(taskList, line);
             }   else if (line.startsWith("todo ")){
                     addToDo(taskList, line);
-
             }   else if (line.startsWith("deadline ")) {
                     addDeadline(taskList, line);
-
             }   else if (line.startsWith("event ")) {
                     addEvent(taskList,line);
 
@@ -105,22 +102,6 @@ public class Duke {
     }
 
     public static void printGoodbye() {
-=======
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < Task.taskCount; i++) {
-                        System.out.println((i + 1) + ". [" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
-                    }
-            }   else if (line.startsWith("done ")) {
-                    int idx = Integer.parseInt(line.substring(5)) - 1;
-                    taskList[idx].markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("\t" + "[" + "\u2713" + "] " + taskList[idx].description);
-            }   else {
-                    taskList[Task.taskCount] = new Task (line);
-                    System.out.println("added: " + line);
-            }
-        }
->>>>>>> 4a7194081ffd73f992f3152e22edc08029ca346b
         System.out.println("Goodbye sir");
         System.out.println(separator);
     }
