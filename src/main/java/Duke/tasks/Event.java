a47e7d9627a3e9472600fe4f97364c1c9470b06a
skyaxe97 <skyaxe97@gmail.com>
2020-09-26 11:20:15 +0800

    Change time from String object to LocalDateTime object

--- src/main/java/Duke/tasks/Event.java
+++ src/main/java/Duke/tasks/Event.java
@@ -1,23 +1,27 @@
 package Duke.tasks;
 
-public class Event extends Task {
-    protected String time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public String getTime() {
+public class Event extends Task {
+    protected LocalDateTime time;
+    public static final DateTimeFormatter format = DateTimeFormatter.ofPattern("MMM d yyyy, HH:mm");
+
+    public LocalDateTime getTime() {
         return this.time;
     }
 
-    public Event(String description, String time) {
+    public Event(String description, LocalDateTime time) {
         super(description);
         this.time = time;
     }
-    public Event(String description, String time, boolean isDone) {
+    public Event(String description, LocalDateTime time, boolean isDone) {
         super(description);
         this.time = time;
         this.isDone = isDone;
     }
     @Override
     public String toString() {
-        return ".[E]" + super.toString() + " (" + time + ")";
+        return ".[E]" + super.toString() + " (" + time.format(format) + ")";
     }
 }
